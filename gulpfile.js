7cfb458e55a0f65b93ce5e923d2997d25ae82a07
AntonEssential <antonessential@hotmail.co.uk>
2016-08-05 21:38:07 +0300

    version 1.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('connect',function(){
 gulp.task('jade',function(){
   gulp.src('jade/*.jade')
     .pipe(jade({
-      pretty: false
+      pretty: true
       }))
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload())
